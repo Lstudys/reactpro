eaf3def20da228323b3dce306b2b50ed436e1ce9
Lstudys <Lstudys@163.com>
2021-11-01 10:44:54 +0800

    数据使用数据库中的数据，bug已解决

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,24 +1,19 @@
 import express from 'express';
 
-import products from '../data/products.js';
+// import products from '../data/products.js';
 import ProductModule from '../models/productModule.js';
 
 const router = express.Router()
 
 
-router.get('/:id',(req,res)=>{
-    const productss = ProductModule.find({});
-    const product = products.find(p=>
-        p._id === req.params.id
-        );
-        // console.log('000000---------------------------0000000');
-        // console.log(productss);
-    res.json(productss);
+router.get('/:id',async (req,res)=>{
+    const products = await ProductModule.findById(req.params.id);
+    res.json(products);
 })
 
 
 router.get('/',async (req,res)=>{
-    // const product = await ProductModule.find({});
+    const products = await ProductModule.find({});
     res.json(products);
 })
 
